e21dde7288023af93d27daabf02df696614a0792
pduo0004 <144980792+pduo0004@users.noreply.github.com>
2023-10-17 04:54:38 +1100

    Java file

---
+++ visualisation.js
@@ -0,0 +1,54 @@
+var vg1 = "geomap2.vg.json";
+vegaEmbed("#geomap_chart", vg1).then(function (result) {
+    
+}).catch(console.error);
+
+var vg2 = "stackedbarchart2.vg.json";
+vegaEmbed("#stackedbar_chart", vg2).then(function (result) {
+    
+}).catch(console.error);
+
+var vg3 = "visualisation1.vg.json";
+vegaEmbed("#geo_chart", vg3).then(function (result) {
+    
+}).catch(console.error);
+
+var vg4 = "multiplebarcharts.vg.json";
+vegaEmbed("#multiple_bar_chart", vg4).then(function (result) {
+    
+}).catch(console.error);
+
+var vg5 = "scatterplot.json";
+vegaEmbed("#scatter_chart", vg5).then(function (result) {
+    
+}).catch(console.error);
+
+var vg6 = "piechart2.vg.json";
+vegaEmbed("#pie_chart_2", vg6).then(function (result) {
+    
+}).catch(console.error);
+
+var vg7 = "piechart3.vg.json";
+vegaEmbed("#pie_chart_3", vg7).then(function (result) {
+    
+}).catch(console.error);
+
+var vg8 = "piechart4.vg.json";
+vegaEmbed("#pie_chart_4", vg8).then(function (result) {
+    
+}).catch(console.error);
+
+var vg9 = "barchart.vg.json";
+vegaEmbed("#bar_chart", vg9).then(function (result) {
+    
+}).catch(console.error);
+
+var vg10 = "barchart2.vg.json";
+vegaEmbed("#bar_chart_2", vg10).then(function (result) {
+    
+}).catch(console.error);
+
+var vg11 = "multiplebarcharts2.vg.json";
+vegaEmbed("#multiple_bar_chart_2", vg11).then(function (result) {
+    
+}).catch(console.error);
